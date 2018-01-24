3b140aae09b16ae0ef5a7b9c63fa9e3028894fb5
Eric V <eric@eevn.io>
2018-01-24 15:09:13 -0800

    Delete udp_listener.py

--- udp_listener.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-sock.bind(('10.100.69.207', 5355))
-while True:
-    data = sock.recv(1024)
-    print data
